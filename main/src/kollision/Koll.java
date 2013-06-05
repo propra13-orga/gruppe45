856ef428d0300eb647e5da3c59dae8f21310c2b3
AndreasRoth <andreas.roth@uni-duesseldorf.de>
2013-06-05 19:03:05 +0200

    Allg. Kollision fertig, Events (Schaden,Tod) fehlen noch

--- main/src/kollision/Koll.java
+++ main/src/kollision/Koll.java
@@ -4,27 +4,61 @@ import spielobjekte.*;
 
 //Spielfeld.obj_list.get(0) = ziel
 //Spielfeld.obj_list.get(1) = hase
+//Spielfeld.obj_list.get(2) = igel
 
 public class Koll {
 	
 	//returns false if player hits exit, easy to run in while-loop
 	public boolean ziel(){
-		if ( (Spielfeld.obj_list.get(1).pos_x < ziel.pos_x+ziel.image.getWidth(null)) && (Spielfeld.obj_list.get(1).pos_x+hase.image.getWidth(null) > ziel_pos_x) && (hase.pos_y < ziel.pos_y+ziel.image.getHeight(null)) && (hase.pos_y+hase.image.getHeight(null) > ziel.pos_y) ){
+		if (   (Spielfeld.obj_list.get(1).pos_x < Spielfeld.obj_list.get(0).pos_x + Spielfeld.obj_list.get(0).image.getWidth(null))
+			&& (Spielfeld.obj_list.get(1).pos_x + Spielfeld.obj_list.get(1).image.getWidth(null) > Spielfeld.obj_list.get(0).pos_x)
+			&& (Spielfeld.obj_list.get(1).pos_y < Spielfeld.obj_list.get(0).pos_y + Spielfeld.obj_list.get(0).image.getHeight(null))
+			&& (Spielfeld.obj_list.get(1).pos_y + Spielfeld.obj_list.get(1).image.getHeight(null) > Spielfeld.obj_list.get(0).pos_y) )
+		{
 			return false;
 		}
 		else return true;
 	}
 	
-	//returns 0 for no collision, 1 for wall/tree, 2 for poisontree, 3 for killerbunny, 4 for player
+	//returns true for no collision, if false, invokes event if tester = killer / poisontree
+	//1 for wall/tree, 2 for poisontree, 3 for killerbunny, 4 for player
 	public Boolean koll(Objekt tester){
-		for(int i; i < Spielfeld.obj_list.size(); i++){
-			if (Main.obj_list[i].nr == tester.nr) continue;
-			else if ( (tester.pos_x < Main.obj_array[i].pos_x+Main.obj_array[i].image.getWidth(null)) && (tester.pos_x+tester.image.getWidth(null) > Main.obj_array[i].pos_x) && (tester.pos_y < Main.obj_array[i].pos_y+Main.obj_array[i].image.getHeight(null)) && (tester.pos_y+tester.image.getHeight(null) > Main.obj_array[i].pos_y) ){
-				Main.obj_array[i].type
+		switch (tester.type) { //Kollision je nach Tester varierend
+			case 3:
+				for(int i=3; i < Spielfeld.obj_list.size(); i++){
+					if (Spielfeld.obj_list.get(i).nr == tester.nr)
+					{
+						continue;
+					}
+					else if ( 	(tester.pos_x < Spielfeld.obj_list.get(i).pos_x + Spielfeld.obj_list.get(i).image.getWidth(null))
+							 && (tester.pos_x + tester.image.getWidth(null) > Spielfeld.obj_list.get(i).pos_x)
+							 && (tester.pos_y < Spielfeld.obj_list.get(i).pos_y + Spielfeld.obj_list.get(i).image.getHeight(null))
+							 && (tester.pos_y + tester.image.getHeight(null) > Spielfeld.obj_list.get(i).pos_y) )
+					{
+						//Event auslösen,
+						return false;
+					}
+				}
 				return true;
-			}
-		//}
-		else return false;		
+
+			default:
+				for(int i=3; i < Spielfeld.obj_list.size(); i++){
+					if (Spielfeld.obj_list.get(i).nr == tester.nr)
+					{
+						continue;
+					}
+					else if ( 	(tester.pos_x < Spielfeld.obj_list.get(i).pos_x + Spielfeld.obj_list.get(i).image.getWidth(null))
+							 && (tester.pos_x + tester.image.getWidth(null) > Spielfeld.obj_list.get(i).pos_x)
+							 && (tester.pos_y < Spielfeld.obj_list.get(i).pos_y + Spielfeld.obj_list.get(i).image.getHeight(null))
+							 && (tester.pos_y + tester.image.getHeight(null) > Spielfeld.obj_list.get(i).pos_y) )
+					{
+						//Event auslösen,
+						return false;
+					}
+				}
+				return true;	
+		}
+			
 	}
 	
 }
