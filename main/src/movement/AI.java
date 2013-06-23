58aa998ba87f4284ea83feda50442d481b65ca07
AndreasRoth <andreas.roth@uni-duesseldorf.de>
2013-06-23 21:36:32 +0200

    Code verschönert, Entfernungsmessung wieder eingebaut

--- main/src/movement/AI.java
+++ main/src/movement/AI.java
@@ -6,8 +6,8 @@ import gameobjects.Figure;
 
 public class AI {
 
-        static int STEP = 5;
-    
+	static int STEP = 5;
+
 	//uses move on all opponents
 	public static void move_all_opp(){
 		for(int i = 5 ; i < Main.obj_list.size() ; i++){
@@ -18,7 +18,7 @@ public class AI {
 	//moves an opponent
 	public static void move(Figure figure){
 		
-		int closest_player = 0;//Range.range(figure);					//index(obj_list) of closest player
+		int closest_player = Range.range(figure);					//index(obj_list) of closest player
 		
 		if(closest_player == 0)										//no players in range
 		{
@@ -38,30 +38,30 @@ public class AI {
 
 	//random movement
 	static void rand_move(Figure figure){
-            if(rand_check(figure))
-            {
-		int rand = Main.rand.nextInt(5);						//get random number between 0 and 7
+		if(rand_check(figure))
+		{
+			int rand = Main.rand.nextInt(5);						//get random number between 0 and 7
 
-		switch(rand){											//random movement
-		case 0:	figure.direction = 'u';
-                    Move.up(figure , STEP);								//up
-				break;
+			switch(rand){											//random movement
+				case 0:	figure.direction = 'u';
+						Move.up(figure , STEP);								//up
+						break;
 
-		case 1:		figure.direction = 'r';
-                    Move.right(figure , STEP);								//right
-				break;
+				case 1:	figure.direction = 'r';
+						Move.right(figure , STEP);								//right
+						break;
 
-		case 2:		figure.direction = 'd';
-                    Move.down(figure , STEP);								//down
-				break;
+				case 2:	figure.direction = 'd';
+						Move.down(figure , STEP);								//down
+						break;
 
-		case 3:		figure.direction = 'l';
-                    Move.left(figure , STEP);								//left
-				break;
+				case 3:	figure.direction = 'l';
+						Move.left(figure , STEP);								//left
+						break;
 
-		case 4:	break;												//too lazy to move
+				case 4:	break;												//too lazy to move
+			}
 		}
-            }
 	}
 	
 	//checkes if movment in "direction" is possible, if not, returns true
@@ -79,35 +79,35 @@ public class AI {
 				break;
 
 				case 'd':
-                    if((figure.pos_y + figure.image.getHeight(null)) > Main.board_height - Main.off - STEP * 2) return true;
-                    else if(Coll.coll(figure, 0, STEP))
-                    {
-                        Move.down(figure, STEP);
-                        return false;
-                    }
-                    break;
+					if((figure.pos_y + figure.image.getHeight(null)) > Main.board_height - Main.off - STEP * 2) return true;
+					else if(Coll.coll(figure, 0, STEP))
+					{
+						Move.down(figure, STEP);
+						return false;
+					}
+					break;
 
-                case 'l':
-                    if(figure.pos_x < Main.off + STEP * 2) return true;
-                    else if(Coll.coll(figure, -STEP, 0))
-                    {
-                        Move.left(figure, STEP);
-                        return false;
-                    }
-                    break;
+				case 'l':
+					if(figure.pos_x < Main.off + STEP * 2) return true;
+					else if(Coll.coll(figure, -STEP, 0))
+					{
+						Move.left(figure, STEP);
+						return false;
+					}
+					break;
 
-                case 'r':
-                    if((figure.pos_x + figure.image.getWidth(null)) > Main.board_width - Main.off - STEP * 2) return true;
-                    else if(Coll.coll(figure, STEP, 0))
-                    {
-                        Move.right(figure, STEP);
-                        return false;
-                    }
-                    break;
-            }
-            return true;
-        }
-        
+				case 'r':
+					if((figure.pos_x + figure.image.getWidth(null)) > Main.board_width - Main.off - STEP * 2) return true;
+					else if(Coll.coll(figure, STEP, 0))
+					{
+						Move.right(figure, STEP);
+						return false;
+					}
+					break;
+			}
+			return true;
+		}
+
 	//intelligent movement towards player
 	static void intel_move(Figure opp , Figure player){
 		
