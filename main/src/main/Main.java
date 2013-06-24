ab1568688c41d1885ca3bdcbeb447269326dfa65
AndreasRoth <andreas.roth@uni-duesseldorf.de>
2013-06-24 18:40:04 +0200

    Gegner geben EP , Items geben HP/MP

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -41,7 +41,7 @@ public static void main(String[] args) throws IOException
 		game.start();												//starts Game logic
 		graphics.MasterFrame window = new graphics.MasterFrame();	//creates RenderFrame
 	
-	//	Gui start = new Gui();										//Starts Gui
+		Gui start = new Gui();										//Starts Gui
 		
 		while(onOff)
 		{
