4475643d6468bd2dd22cd733464855494737a2d5
JessicaBa <jessica.bachmann@uni-duesseldorf.de>
2013-06-25 16:28:37 +0200

    Boss pics angepasst

--- main/src/gameobjects/Boss.java
+++ main/src/gameobjects/Boss.java
@@ -8,7 +8,7 @@ import local.Fs;
 public class Boss extends Figure{
 
 	public Boss(int x, int y){
-		super(5,x,y,Toolkit.getDefaultToolkit().getImage(Fs.img_pfad + "boss_l.png"));
+		super(5,x,y,local.Pics.boss_l);
 		this.destroyable = true;
 		this.hp = 10;
 		this.bugs = 10;
