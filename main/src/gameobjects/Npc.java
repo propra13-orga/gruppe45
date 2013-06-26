f37c9f8788d0e03afbb5e12129f2ff656851e914
JessicaBa <jessica.bachmann@uni-duesseldorf.de>
2013-06-26 18:19:25 +0200

    Npc Pics

--- main/src/gameobjects/Npc.java
+++ main/src/gameobjects/Npc.java
@@ -1,12 +1,9 @@
 package gameobjects;
 
-import java.awt.Toolkit;
-
-import local.Fs;
-
 public class Npc extends Figure{
 
 	public Npc(int x, int y){
-		super(6,x,y,Toolkit.getDefaultToolkit().getImage(Fs.img_pfad + "sign.png"));
+		super(6,x,y,local.Pics.sign);
+		//super(6,x,y,Toolkit.getDefaultToolkit().getImage(Fs.img_pfad + "sign.png"));
 	}
 }
