981ebe71db2a3965433eba28f7f00c6d8f2b5291
PolyPalette <martin.knonsalla@uni-duesseldorf.de>
2013-07-11 15:01:45 +0200

    added missing Pics class

---
+++ main/src/local/Pics.java
@@ -0,0 +1,70 @@
+package local;
+
+import java.io.File;
+import java.io.IOException;
+import java.awt.*;
+import javax.imageio.ImageIO;
+//please do no changes!!! still in progress
+public class Pics{
+		public static Image	icon1, bunny_l_motion, bunny_l, bunny_r_motion, hedgehog_l, hedgehog_r, bunny_r,
+							back, tree, bush, rock, fox_r, cave, mole, shop_inactive, shop_active, poisonous_tree,
+							sign, healthbar_green, healthbar_yellow, healthbar_red, lives_img, fox_l, hero2, icon2,
+							life, bug, blob, fireball, fireball_r, fireball_d, fireball_l, fireball_u, peace,
+							attention, flower, boss_l, boss_r, goal, hole, mp, hp;
+	
+	public static void loadPics(){
+	
+	try
+	{
+		icon1 = ImageIO.read(new File (local.Fs.img_pfad+"icon_bunny.png"));
+		icon2 = ImageIO.read(new File (local.Fs.img_pfad+"bunny_l.png"));
+		bunny_l = ImageIO.read(new File (local.Fs.img_pfad+"bunny_l.png"));
+		bunny_r = ImageIO.read(new File (local.Fs.img_pfad+"bunny_r.png"));
+		bunny_l_motion = ImageIO.read(new File(local.Fs.img_pfad+"bunny_l_motion.png"));
+		bunny_r_motion = ImageIO.read(new File(local.Fs.img_pfad+"bunny_r_motion.png"));
+		fireball = ImageIO.read(new File (local.Fs.img_pfad+"icon_fireball.png"));
+		fireball_u = ImageIO.read(new File (local.Fs.img_pfad+"fireball_u.png"));
+		fireball_r = ImageIO.read(new File (local.Fs.img_pfad+"fireball_r.png"));
+		fireball_d = ImageIO.read(new File (local.Fs.img_pfad+"fireball_d.png"));
+		fireball_l = ImageIO.read(new File (local.Fs.img_pfad+"fireball_l.png"));
+		life = ImageIO.read(new File (local.Fs.img_pfad+"heart.png"));
+		blob = ImageIO.read(new File (local.Fs.img_pfad+"blob.png"));
+		peace = ImageIO.read(new File (local.Fs.img_pfad+"peace.jpg"));
+		attention = ImageIO.read(new File (local.Fs.img_pfad+"attention.png"));
+		flower = ImageIO.read(new File (local.Fs.img_pfad+"flower_purple.png"));
+		boss_l = ImageIO.read(new File (local.Fs.img_pfad+"boss_l.png"));
+		boss_r = ImageIO.read(new File (local.Fs.img_pfad+"boss_r.png"));
+		hero2 = ImageIO.read(new File (local.Fs.img_pfad+"bg_menue.png"));
+		fox_l = ImageIO.read(new File (local.Fs.img_pfad+"fox_l.png"));
+		fox_r = ImageIO.read(new File (local.Fs.img_pfad+"fox_r.png"));
+		lives_img = ImageIO.read(new File (local.Fs.img_pfad+"lives_bunny.png"));
+		healthbar_green = ImageIO.read(new File (local.Fs.img_pfad+"healthbar_green.png"));
+		healthbar_yellow = ImageIO.read(new File (local.Fs.img_pfad+"healthbar_yellow.png"));
+		healthbar_red = ImageIO.read(new File (local.Fs.img_pfad+"healthbar_red.png"));
+		poisonous_tree = ImageIO.read(new File (local.Fs.img_pfad+"poisonous_tree.png"));
+		sign = ImageIO.read(new File (local.Fs.img_pfad+"sign.png"));
+		shop_active = ImageIO.read(new File (local.Fs.img_pfad+"shop_active.png"));
+		shop_inactive = ImageIO.read(new File (local.Fs.img_pfad+"shop_inactive.png"));
+		mole = ImageIO.read(new File (local.Fs.img_pfad+"shop_active.png"));
+		cave = ImageIO.read(new File (local.Fs.img_pfad+"cave.jpg"));		
+		tree = ImageIO.read(new File (local.Fs.img_pfad+"wall_1.png"));
+		bush = ImageIO.read(new File (local.Fs.img_pfad+"wall_2.png"));
+		rock = ImageIO.read(new File (local.Fs.img_pfad+"wall_3.png"));
+		back = ImageIO.read(new File (local.Fs.img_pfad+"bg_1_1.png"));
+		hedgehog_l = ImageIO.read(new File (local.Fs.img_pfad+"hedgehog_l.png"));
+		hedgehog_r = ImageIO.read(new File (local.Fs.img_pfad+"hedgehog_r.png"));
+		bug = ImageIO.read(new File (local.Fs.img_pfad+"bug.png"));
+		goal = ImageIO.read(new File (local.Fs.img_pfad+"goal.png"));
+		hole = ImageIO.read(new File (local.Fs.img_pfad+"goal_2_3.png"));
+		hp = ImageIO.read(new File (local.Fs.img_pfad+"medipack.png"));
+		mp = ImageIO.read(new File (local.Fs.img_pfad+"candy_blue.png"));
+	}
+		
+	catch(IOException e)
+		{	//TODO Auto-generated block		}
+		System.out.println("Bild aus pic.loadImage() kann nicht eingelesen werden");
+		}
+	}
+	
+}
+
